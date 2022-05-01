123f780d08e691876f393557e7c6fe3f34287d57
bradwasher <13262221+bradwasher@users.noreply.github.com>
2022-05-01 12:52:40 -0400

    Create README.m

---
+++ README.m
@@ -0,0 +1,8 @@
+pop is a point of presence detector that wraps nmap and uses a colon separated list of points of presence with the name, ip, and port
+
+eg
+
+Drop Bear:127.0.0.1:443
+Tinyshell:8.8.8.8:53
+SSH:127.0.0.1:22
+Meterpreter:127.0.0.0:53
